29b441cbae0dd2d82931297e2ebd68b158c5a7af
Terry Robinson <terry@saratez.co.uk>
2017-01-17 19:54:20 +0000

    Test that spare followed by spare is calculated correctly

--- Bowling/BowlingConsoleTests/BowlingGameTests.cs
+++ Bowling/BowlingConsoleTests/BowlingGameTests.cs
@@ -19,6 +19,7 @@ namespace BowlingConsoleTests
         [TestCase("X|34", 24)]
         [TestCase("63|X|34", 33)]
         [TestCase("3/|4-", 18)]
+        [TestCase("3/|4/|5-", 34)]
         public void ScoreGame(string gameScore, int expectedScore)
         {
             var game = new BowlingGame();
